db826b9aa05082047d7f501093764852bc5f47f5
Julian Rixner <julirix@hotmail.de>
2020-07-23 21:18:52 +0200

    migration

--- frontend/naklario/src/app/app.module.ts
+++ frontend/naklario/src/app/app.module.ts
@@ -3,7 +3,7 @@ import {
   BrowserTransferStateModule,
 } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { AppRoutingModule } from "./app-routing.module";
+import { AppRoutingModule } from '@src/app/app-routing.module';
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
@@ -13,28 +13,28 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { LoadingBarRouterModule } from "@ngx-loading-bar/router";
 import { LoadingBarHttpClientModule } from "@ngx-loading-bar/http-client";
 
-import { JwtInterceptor, ErrorInterceptor } from "./_helpers";
+import { JwtInterceptor, ErrorInterceptor } from '@src/app/_helpers';
 
 // Components
-import { AppComponent } from "./app.component";
-import { AccountModule } from "./account/account.module";
-import { RouletteModule } from "./roulette/roulette.module";
+import { AppComponent } from '@src/app/app.component';
+import { AccountModule } from '@src/app/account/account.module';
+import { RouletteModule } from '@src/app/roulette/roulette.module';
 
-import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./home/misc/about/about.component";
-import { DatenschutzComponent } from "./home/misc/datenschutz/datenschutz.component";
-import { HelpSupportComponent } from "./home/misc/help-support/help-support.component";
-import { FeedbackComponent } from "./home/misc/feedback/feedback.component";
-import { ImpressumComponent } from "./home/misc/impressum/impressum.component";
-import { ParentsComponent } from "./home/misc/parents/parents.component";
-import { PrivacyComponent } from "./home/misc/privacy/privacy.component";
-import { SchoolsComponent } from "./home/misc/schools/schools.component";
-import { StudentsComponent } from "./home/misc/students/students.component";
-import { TutorsComponent } from "./home/misc/tutors/tutors.component";
-import { TermsConditionsComponent } from "./home/misc/terms-conditions/terms-conditions.component";
-import { FooterComponent } from "./home/misc/footer/footer.component";
-import { NavbarComponent } from "./home/misc/navbar/navbar.component";
-import { PageNotFoundComponent } from "./home/misc/page-not-found/page-not-found.component";
+import { HomeComponent } from '@src/app/home/home.component';
+import { AboutComponent } from '@src/app/home/misc/about/about.component';
+import { DatenschutzComponent } from '@src/app/home/misc/datenschutz/datenschutz.component';
+import { HelpSupportComponent } from '@src/app/home/misc/help-support/help-support.component';
+import { FeedbackComponent } from '@src/app/home/misc/feedback/feedback.component';
+import { ImpressumComponent } from '@src/app/home/misc/impressum/impressum.component';
+import { ParentsComponent } from '@src/app/home/misc/parents/parents.component';
+import { PrivacyComponent } from '@src/app/home/misc/privacy/privacy.component';
+import { SchoolsComponent } from '@src/app/home/misc/schools/schools.component';
+import { StudentsComponent } from '@src/app/home/misc/students/students.component';
+import { TutorsComponent } from '@src/app/home/misc/tutors/tutors.component';
+import { TermsConditionsComponent } from '@src/app/home/misc/terms-conditions/terms-conditions.component';
+import { FooterComponent } from '@src/app/home/misc/footer/footer.component';
+import { NavbarComponent } from '@src/app/home/misc/navbar/navbar.component';
+import { PageNotFoundComponent } from '@src/app/home/misc/page-not-found/page-not-found.component';
 
 import {
   DatabaseService,
@@ -42,22 +42,23 @@ import {
   ToastService,
   RouletteService,
   BannerService,
-} from "./_services";
+} from '@src/app/_services';
 
-import { MiscComponentsModule } from "./_misc_components/misc-components.module";
-import { MoreInformationComponent } from "./home/more-information/more-information.component";
-import { BannerComponent } from "./home/misc/banner/banner.component";
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { ScrollSpyDirective } from './_helpers/scroll-spy.directive';
-import { PartnersComponent } from './home/more-information/partners/partners.component';
-import { FeaturesComponent } from './home/more-information/features/features.component';
-import { VideoComponent } from './home/more-information/video/video.component';
-import { MissionComponent } from './home/more-information/mission/mission.component';
-import { NumbersComponent } from './home/more-information/numbers/numbers.component';
-import { TeamComponent } from './home/more-information/team/team.component';
-import { JoinTheCommunityComponent } from './home/more-information/join-the-community/join-the-community.component';
-import { SocialIconsComponent } from './home/misc/social-icons/social-icons.component';
-import { PressComponent } from './home/misc/press/press.component';
+import { MiscComponentsModule } from '@src/app/_misc_components/misc-components.module';
+import { MoreInformationComponent } from '@src/app/home/more-information/more-information.component';
+import { BannerComponent } from '@src/app/home/misc/banner/banner.component';
+import { DashboardComponent } from '@src/app/dashboard/dashboard.component';
+import { ScrollSpyDirective } from '@src/app/_helpers/scroll-spy.directive';
+import { PartnersComponent } from '@src/app/home/more-information/partners/partners.component';
+import { FeaturesComponent } from '@src/app/home/more-information/features/features.component';
+import { VideoComponent } from '@src/app/home/more-information/video/video.component';
+import { MissionComponent } from '@src/app/home/more-information/mission/mission.component';
+import { NumbersComponent } from '@src/app/home/more-information/numbers/numbers.component';
+import { TeamComponent } from '@src/app/home/more-information/team/team.component';
+import { JoinTheCommunityComponent } from '@src/app/home/more-information/join-the-community/join-the-community.component';
+import { SocialIconsComponent } from '@src/app/home/misc/social-icons/social-icons.component';
+import { PressComponent } from '@src/app/home/misc/press/press.component';
+import { AutoGeneratedComponent } from '@src/app/auto-generated/auto-generated.component';
 
 @NgModule({
   declarations: [
@@ -90,6 +91,7 @@ import { PressComponent } from './home/misc/press/press.component';
     JoinTheCommunityComponent,
     SocialIconsComponent,
     PressComponent,
+    AutoGeneratedComponent,
   ],
   imports: [
     NgbModule,
